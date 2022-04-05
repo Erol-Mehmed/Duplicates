c2084555328e6f0ccc352c9acbaf2e68f1d473c5
Erol-web <81830899+Erol-web@users.noreply.github.com>
2022-04-05 11:44:20 +0300

    refactor

--- duplicates.js
+++ duplicates.js
@@ -1,75 +1,35 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function htmlInput() {
+  // This function gets the user input.
 
   let inputString = document.getElementById("input").value;
 
   stringCheck(inputString);
-
 }
 
 function stringCheck(inputString) {
+  // This function checks if the input string meets the requirements.
 
   let lengthOfStr = inputString.length;
 
   let bool = true;
 
   if (lengthOfStr >= 2) {
+    let arrOfInput = inputString.split("");
 
-    let arrOfInput = inputString.split('');
-
-    arrOfInput.forEach(el => el !== ' ' ? bool = false : false);
-
+    arrOfInput.forEach((el) => (el !== " " ? (bool = false) : false));
   }
 
   if (lengthOfStr < 2 || bool) {
     document.getElementById("myForm").reset();
 
     return alert("Enter a word at least two character long, please!");
-
   } else {
-
-    displayResult(inputString);
-
+    displayInputString(inputString);
   }
-
 }
 
-function displayResult(inputString) {
+function displayInputString(inputString) {
+  // This function displays the string that the user inputted.
 
   document.getElementById("myForm").reset();
 
@@ -77,60 +37,67 @@ function displayResult(inputString) {
 
   document.getElementById("output").innerHTML = "";
 
-  some(inputString);
-
+  arrayCreation(inputString);
 }
 
-function some(inputString) {
+function arrayCreation(inputString) {
+  // This function transforms the input string into an array.
+  // And turns all characters into lowercase.
 
-  let arrOfInpStr = inputString.split("").filter(chr => chr !== ' ');
+  let arrOfInpStr = inputString.split("").filter((chr) => chr !== " ");
 
-  arrOfInpStr = arrOfInpStr.map(chr => chr.toLowerCase());
+  arrOfInpStr = arrOfInpStr.map((chr) => chr.toLowerCase());
 
-  let length = arrOfInpStr.length;
+  objectCreation(arrOfInpStr);
+}
 
-  let result = [];
+function objectCreation(arrOfInpStr) {
+  // This function creates object from the characters of the input string.
+  // The characters are properties of the object and the values are how many times they are present in the input string.
 
   let object = {};
 
-  arrOfInpStr.forEach(chr => {
-
+  arrOfInpStr.forEach((chr) => {
     if (!object.hasOwnProperty(chr)) {
-
       object[chr] = 1;
-
     } else {
-
       object[chr] += 1;
-
     }
-
   });
 
-
-  for (let chr in object) {
-
-    if (object[chr] > 1) {
-
-      result.push(`${chr} occurs ${object[chr]} times<br>`);
-
-    }
-
-  }
-
-  console.log(result);
-
-  document.getElementById("output").innerHTML = result.join('');
-
+  fillingResult(object);
 }
 
+function fillingResult(object) {
+  // This function fills the result array with the strings to be displayed.
 
-let submitElement = document.getElementById('submit');
+  let result = [];
 
-submitElement.addEventListener('click', e => {
+  for (let chr in object) {
+    if (object[chr] > 1) {
+      result.push(`${chr} occurs ${object[chr]} times<br>`);
+    }
+  }
 
+  displayResult(result);
+}
+
+function displayResult(result) {
+  // This function either displays the content of the result array or that there are no duplicates in the input string.
+
+  if (result.length > 0) {
+    document.getElementById("output").innerHTML = result.join("");
+  } else {
+    document.getElementById("output").innerHTML = `No duplicates`;
+  }
+}
+
+// The code below gets the submit button element and attach an event listener to it.
+
+let submitElement = document.getElementById("submit");
+
+submitElement.addEventListener("click", (e) => {
   e.preventDefault();
 
   htmlInput();
-
 });
